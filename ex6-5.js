139ec74ff81c46d20c57f33343cb429341feba62
kojimafuu <zm18514@josai.ac.jp>
2020-12-22 02:09:20 +0000

    12.22

---
+++ ex6-5.js
@@ -0,0 +1,6 @@
+var who65 = 'world';function sayhello65() {alert('Hello, ' + who65 + '!');}
+function taro65() {who65 = '太郎';}
+function hanako65() {who65 = '花子';}
+var who66 = 'world';function sayhello66() {alert('Hello, ' + who66 + '!');}
+function taro66() {who66 = '太郎';alert(who66+'がきた');}
+function hanako66() {who66 = '花子';alert(who66+'がきた');}
